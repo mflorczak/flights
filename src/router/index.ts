f08c0358da77ae4b784f8e936d602bcaef1f71aa
Michał Florczak <michal.florczak@fideltronik.com>
2020-05-01 11:36:38 +0200

    Added auth guard and Login route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import Home from "../views/Home.vue";
+import Login from '../views/Login.vue'
+import users from '@/store/modules/users';
 
 Vue.use(VueRouter);
 
@@ -8,21 +10,43 @@ const routes: Array<RouteConfig> = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
+    meta: {
+      auth: true
+    }
   },
   {
     path: "/about",
     name: "About",
+    meta: {
+      auth: true
+    },
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
   }
 ];
 
 const router = new VueRouter({
   routes
 });
+
+router.beforeEach((to, from, next) => {
+  console.log('bronimy')
+  const accessToken = users.accessToken
+  console.log(accessToken)
+  if (to.meta.auth && !accessToken) {
+    next('/login')
+  } else {
+    next()
+  }
+})
 
 export default router;
