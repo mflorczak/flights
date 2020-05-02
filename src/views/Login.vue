8e6d6fca77eb68202b977a900e1d4ad1015e2b64
Michał Florczak <michal.florczak@fideltronik.com>
2020-05-02 11:40:12 +0200

    Added logo and styles

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,36 +1,32 @@
 <template>
-  <form action="">
-    <div class="modal-card" style="width: auto">
-      <header class="modal-card-head">
-        <p class="modal-card-title">Login</p>
-      </header>
-      <section class="modal-card-body">
-        <b-field label="Email or username">
-          <b-input
-            v-model="usernameOrEmail"
-            placeholder="Your email"
-            required
-          >
-          </b-input>
-        </b-field>
+  <div class="login-container">
+    <div class="login-form">
+      <div class="logo">
+        <img src="../assets/password-login-svgrepo-com.svg">
+      </div>
+      <form action="" class="form">
+        <b-input class="input-field"
+          v-model="usernameOrEmail"
+          placeholder="Twój login lub email"
+          required
+        >
+        </b-input>
 
-        <b-field label="Password">
-          <b-input
-            type="password"
-            v-model="password"
-            password-reveal
-            placeholder="Your password"
-            required
-          >
-          </b-input>
-        </b-field>
-
-      </section>
-      <footer class="modal-card-foot">
-        <button class="button is-primary" @click="login">Login</button>
-      </footer>
+        <b-input class="input-field"
+          type="password"
+          v-model="password"
+          password-reveal
+          placeholder="Twoje hasło"
+          required
+        >
+        </b-input>
+      <div class="button-placement">
+        <button class="button login-button" :disabled="password.length < 1 || usernameOrEmail.length < 1" @click="login">Zaloguj</button>
+        <button class="button registration-button" @click="register">Rejestracja</button>
+      </div>
+      </form>
     </div>
-  </form>
+  </div>
 </template>
 
 <script lang="ts">
@@ -56,5 +52,57 @@ export default class Login extends Vue {
       .then(() => this.$router.push('/'))
       .catch(error => console.log(error.response.data.message))
   }
+
+  register() {
+    this.$router.push('/register')
+  }
 }
 </script>
+
+
+<style lang="sass" scoped>
+.login-form
+  position: absolute
+  top: 50%
+  left: 50%
+  transform: translate(-50%, -50%)
+.logo
+  height: 300px
+  width: 300px
+  display: inline-block
+  margin: 0 auto
+  padding: 3px
+  .form
+    display: flex
+    flex-direction: column
+    justify-content: column
+
+.login-container
+  position: absolute
+  width: 100vw
+  height: 100vh
+  background: linear-gradient(180deg, #52B0F4 0%, rgba(169, 205, 232, 0) 100%);
+
+.registration-button
+  width: 282px
+  height: 47px
+  margin: 5px
+
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25)
+
+  background: #21D0C3
+  border-radius: 20px
+
+.login-button
+  @extend .registration-button
+  background: #A6A3A3
+
+.input-field
+  width: 282px
+  height: 47px
+  margin: 0 auto
+  padding: 10px
+
+.button-placement
+  display: inline-grid
+</style>
