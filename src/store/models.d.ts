1882359d03368915e7f1662039965569ec2bdef8
Michał Florczak <michal.florczak@fideltronik.com>
2020-05-02 11:39:31 +0200

    Added register models

--- src/store/models.d.ts
+++ src/store/models.d.ts
@@ -12,3 +12,16 @@ export interface User {
 export interface UserResponse {
   user: User
 }
+
+export interface UserRegister {
+  name: string,
+  surname: string,
+  email: string,
+  password: string,
+  username: string
+}
+
+export interface ApiResponse {
+  success: boolean,
+  message: string
+}
