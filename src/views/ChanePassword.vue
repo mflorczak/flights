1d7c83f32f1f29e5108188a7d9742e8f582ee1a0
Michał Florczak <michal.florczak@fideltronik.com>
2020-05-07 18:41:29 +0200

    Added new change password component

--- src/views/ChanePassword.vue
+++ src/views/ChanePassword.vue
@@ -1,33 +1,57 @@
 <template>
- <div class="login-container">
-    <div class="login-form">
-      <div class="logo">
-        <img src="../assets/password-login-svgrepo-com.svg">
-      </div>
-      <form action="" class="form">
-        <ValidationObserver v-slot="{ invalid }">
+ <!-- <div class="login-container">
+    <form>
+      <section>
+      <ValidationObserver v-slot="{ invalid }" class="login-form">
 
-          <ValidationProvider rules="required" vid="password" v-slot="{ errors, valid }">
-            <b-field :type="{ 'is-danger': errors[0], 'is-success': valid }" :message="errors">
-              <b-input class="input-field" type="password" v-model="password" placeholder="Twoje hasło" required></b-input>
-            </b-field>
-          </ValidationProvider>
+        <ValidationProvider rules="required" vid="password" v-slot="{ errors, valid }">
+          <b-field :type="{ 'is-danger': errors[0], 'is-success': valid }" :message="errors">
+            <b-input class="input-field" type="password" v-model="password" placeholder="Twoje hasło" required></b-input>
+          </b-field>
+        </ValidationProvider>
 
-          <ValidationProvider rules="required|confirmed:password" v-slot="{ errors, valid }">
-            <b-field :type="{ 'is-danger': errors[0], 'is-success': valid }" :message="errors">
-              <b-input class="input-field" type="password" v-model="theSamePassword" placeholder="Powtórz hasło" required> </b-input>
-            </b-field>
-          </ValidationProvider>
+        <ValidationProvider rules="required|confirmed:password" v-slot="{ errors, valid }">
+          <b-field :type="{ 'is-danger': errors[0], 'is-success': valid }" :message="errors">
+            <b-input class="input-field" type="password" v-model="theSamePassword" placeholder="Powtórz hasło" required> </b-input>
+          </b-field>
+        </ValidationProvider>
 
-          <div class="button-placement">
-            <button class="button change-password-button" :disabled="invalid" @click="changePassword()">Zmień hasło</button>
-            <button type="button" class="button cancel-button is-danger" @click="$parent.close()">Anuluj</button>
-          </div>
+        <button class="button change-password-button" :disabled="invalid" @click="changePassword()">Zmień hasło</button>
+        <button type="button" class="button cancel-button is-danger" @click="$parent.close()">Anuluj</button>
 
+      </ValidationObserver>
+      </section>
+    </form>
+  </div> -->
+<form action="">
+    <div class="modal-card" style="width: auto">
+        <header class="modal-card-head">
+            <p class="modal-card-title">Zmiana hasła</p>
+        </header>
+          <ValidationObserver v-slot="{ invalid }" class="login-form">
+        <section class="modal-card-body">
+
+
+      <ValidationProvider rules="required" vid="password" v-slot="{ errors, valid }">
+        <b-field :type="{ 'is-danger': errors[0], 'is-success': valid }" :message="errors" label="Hasło">
+          <b-input class="input-field" type="password" v-model="password" placeholder="Twoje hasło" required></b-input>
+        </b-field>
+      </ValidationProvider>
+
+      <ValidationProvider rules="required|confirmed:password" v-slot="{ errors, valid }">
+        <b-field :type="{ 'is-danger': errors[0], 'is-success': valid }" :message="errors" label="Powtórzone hasło">
+          <b-input class="input-field" type="password" v-model="theSamePassword" placeholder="Powtórz hasło" required> </b-input>
+        </b-field>
+      </ValidationProvider>
+
+        </section>
+        <footer class="modal-card-foot button-foot">
+          <button class="button change-password-button" :disabled="invalid" @click="changePassword()">Zmień hasło</button>
+          <button type="button" class="button cancel-button" @click="$parent.close()">Anuluj</button>
+        </footer>
         </ValidationObserver>
-      </form>
     </div>
-  </div>
+</form>
 </template>
 
 <script lang="ts">
@@ -59,53 +83,26 @@ export default class ChangePassword extends Vue {
   changePassword() {
     changePassword(this.userEmail, this.password)
     .then(respone => {console.log(respone); this.close()})
-    .catch(error => console.log(error.response.data.message))
+    .catch(error => {console.log(error.response.data.message); this.close()})
   }
 }
 </script>
 
 <style lang="sass" scoped>
-.login-form
-  position: absolute
-  top: 50%
-  left: 50%
-  transform: translate(-50%, -50%)
-.logo
-  height: 300px
-  width: 300px
-  display: inline-block
-  margin: 0 auto
-  padding: 3px
-  .form
-    display: flex
-    flex-direction: column
-    justify-content: column
 
-.login-container
-  position: absolute
-  // width: 100vw
-  // height: 100vh
-  // background: linear-gradient(180deg, #52B0F4 0%, rgba(169, 205, 232, 0) 100%);
+.button-foot
+  display: flex
+  flex-flow: row wrap
+  justify-content: space-between
+  align-items: center
 
 .change-password-button
-  width: 282px
-  height: 47px
-  margin: 5px
-
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25)
-
   background: #21D0C3
   border-radius: 20px
+  width: 100px
 
 .cancel-button
   @extend .change-password-button
-
-
-.input-field
-  width: 282px
-  height: 47px
-  margin: 0 auto
-
-.button-placement
-  display: inline-grid
+  background: #e63946
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,36 +1,21 @@
 <template>
-  <div class="container">
-      <b-navbar type="is-info" :mobile-burger="false">
-        <template slot="brand">
-            <b-navbar-item tag="router-link" :to="{ path: '/' }">
-                <img
-                    src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
-                    alt="Lightweight UI components for Vue.js based on Bulma"
-                >
-            </b-navbar-item>
-        </template>
-        <template slot="end">
-            <b-navbar-item tag="div">
-                <b-dropdown aria-role="menu" trap-focus position="is-bottom-left">
-                <button class="button" slot="trigger" slot-scope="{ active }">
-                  <b-icon pack="fas" icon="user" size="is-large"></b-icon>
-                  <span>{{userEmail}}</span>
-                  <b-icon pack="fas" :icon="active ? 'caret-up' : 'caret-down'" size="is-large"></b-icon>
-                </button>
-                <b-dropdown-item aria-role="listitem" @click="openChangePasswordModal()">
-                  <b-icon pack="fas" icon="key" size="is-medium"></b-icon>
-                  <span>Zmień hasło</span>
-                </b-dropdown-item>
-                <b-dropdown-item aria-role="listitem" @click="signout()">
-                  <b-icon pack="fas" icon="sign-out-alt" size="is-medium"></b-icon>
-                  <span>Wyloguj</span>
-                </b-dropdown-item>
-                </b-dropdown>
-            </b-navbar-item>
-        </template>
-    </b-navbar>
-    <button @click="test()">Button</button>
-    </div>
+  <nav class="navbar">
+    <b-dropdown aria-role="menu" trap-focus style="margin-right: 6px" position="is-bottom-left">
+      <button class="button" slot="trigger" slot-scope="{ active }">
+        <b-icon pack="fas" icon="user" size="is-large"></b-icon>
+        <span>{{userEmail}}</span>
+        <b-icon pack="fas" :icon="active ? 'caret-up' : 'caret-down'" size="is-large"></b-icon>
+      </button>
+      <b-dropdown-item aria-role="listitem" @click="openChangePasswordModal()">
+        <b-icon pack="fas" icon="key" size="is-medium"></b-icon>
+        <span>Zmień hasło</span>
+      </b-dropdown-item>
+      <b-dropdown-item aria-role="listitem" @click="signout()">
+        <b-icon pack="fas" icon="sign-out-alt" size="is-medium"></b-icon>
+        <span>Wyloguj</span>
+      </b-dropdown-item>
+    </b-dropdown>
+  </nav>
 </template>
 
 <script lang="ts">
@@ -63,10 +48,19 @@ export default class Home extends Vue {
       component: ChangePassword,
       hasModalCard: true,
       canCancel: false,
-      customClass: 'custom-class custom-class-2',
       trapFocus: true
     })
   }
 
 }
 </script>
+
+<style lang="sass" scoped>
+  .navbar
+    height: 50px
+    display: flex
+    flex-flow: row nowrap
+    justify-content: flex-end
+    align-items: center
+    background: red
+</style>
