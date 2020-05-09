e26fb24e54ecdc31867ea31df5bf97bf437d5732
Michał Florczak <michal.florczak@fideltronik.com>
2020-05-09 08:58:02 +0200

    Added calendar component

---
+++ src/components/PickDate.vue
@@ -0,0 +1,26 @@
+<template>
+  <b-field label="Okres pobytu">
+    <b-datepicker placeholder="Wybierz date wylotu oraz powrotu" v-model="dates" range @input="datesRange"> </b-datepicker>
+    {{dates}}
+  </b-field>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import { Getter, Action } from 'vuex-class'
+import { City } from '../store/models'
+import { Emit } from 'vue-property-decorator'
+
+@Component
+export default class PickDate extends Vue {
+  private dates: Date[] = []
+
+  @Emit()
+  public datesRange() {
+    return this.dates;
+  }
+}
+</script>
+
+<style lang="sass" scoped></style>
