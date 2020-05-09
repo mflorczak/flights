621f9e5f9e2cba67aa3e67829d9c04f6df268a33
Michał Florczak <michal.florczak@fideltronik.com>
2020-05-09 20:09:37 +0200

    Changed redairect connection path

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   lintOnSave: false,
   devServer: {
     proxy: {
-     '/*': {
+     '/api/*': {
         target: 'http://localhost:8080',
         secure: true,
         changeOrigin:true,
