d01062d9abcbdf35f2dc1f80e8b8e28b0c0a39a4
Michał Florczak <michal.florczak@fideltronik.com>
2020-05-09 20:15:07 +0200

    Added new view

---
+++ src/views/Flights.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <Navbar/>
+  <PresentBookedFlights/>
+</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import { Getter, Action } from 'vuex-class'
+import ChangePassword from './ChanePassword.vue'
+import Navbar from '../components/Navbar.vue'
+import PresentBookedFlights from '../components/PresentBookedFlights.vue'
+
+@Component({
+  components: {
+    Navbar,
+    PresentBookedFlights
+  }
+})
+export default class Flights extends Vue {
+}
+</script>
